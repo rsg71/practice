557c83501f740e42917af97332b702bb2b1fae7e
Robert Greenawalt <robert.greenawalt29@gmail.com>
2020-11-09 23:19:01 -0500

    deleting extraneous comment

--- create-react-express/routes/apiRoutes.js
+++ create-react-express/routes/apiRoutes.js
@@ -3,8 +3,7 @@ const db = require("../models");
 
 router.get("/orders", (req, res) => {
     console.log("on the backend")
-  // Use a regular expression to search titles for req.query.q
-  // using case insensitive match. https://docs.mongodb.com/manual/reference/operator/query/regex/index.html
+    
   db.Order.findAll({
 
   })
